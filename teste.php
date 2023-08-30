adae1b880df768fe94c57595cfaedaa2b08811ed
Humberto Patrick Lacerda Ribeiro <humbertop@liae.edu.uniube.br>
2023-08-29 21:51:07 -0300

    alterando conflitos

--- teste.php
+++ teste.php
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
 teste ttttt o professor Patrick é o melhor!
-=======
 teste alkfj alsdfj laskdfj lçasdfj sadjflsdaj flsdakj flsad jçasld jgsdlj lç
->>>>>>> c10889b98fe27091fcbfeb6e662d9ade1b694920
